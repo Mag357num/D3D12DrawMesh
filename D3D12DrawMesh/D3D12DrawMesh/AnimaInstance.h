a52f2169b581564b490c8fc1c8c1a9eb5ddf929b
mag357num <45219797+Mag357num@users.noreply.github.com>
2021-05-12 14:12:47 +0800

    changes: add animainstance class

---
+++ D3D12DrawMesh/D3D12DrawMesh/AnimaInstance.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "stdafx.h"
+#include "Joint.h"
+
+using FPose = FTransform;
+
+class FSkeleton
+{
+private:
+	vector<FJoint> Joints;
+	vector<FPose> BindPoses;
+	std::unordered_map<string, uint16> NameToIndexMap;
+
+public:
+	FSkeleton() = default;
+	~FSkeleton() = default;
+
+private:
+
+};
+
