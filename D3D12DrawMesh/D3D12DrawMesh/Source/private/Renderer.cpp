1143f0cfc8fff77c82c74342f65b195c2d21b1d9
mag357num <45219797+Mag357num@users.noreply.github.com>
2021-06-17 10:24:15 +0800

    changes: add SCOPED_EVENT for RenderScene()

--- D3D12DrawMesh/D3D12DrawMesh/Source/private/Renderer.cpp
+++ D3D12DrawMesh/D3D12DrawMesh/Source/private/Renderer.cpp
@@ -73,105 +73,112 @@ void FRenderer::RenderScene(FDynamicRHI* RHI, const uint32& FrameIndex, FSingleB
 	// scene color pass
 	{
 		SCOPED_EVENT("SceneColor");
-		RHI->SetTextureState(SFrameRes.SceneColorMap.get(), FRESOURCE_STATES::RESOURCE_STATE_RENDER_TARGET);
-		RHI->ClearRenderTarget(SFrameRes.SceneColorMap->RtvHandle.get());
-		RHI->SetRenderTarget(1, Rt, SFrameRes.DepthStencilMap->DsvHandle.get());
-		RHI->ClearDepthStencil(SFrameRes.DepthStencilMap.get());
-		RHI->SetViewport(0.0f, 0.0f, static_cast<float>(Width), static_cast<float>(Height), 0.f, 1.f);
-		RHI->SetScissor(0, 0, Width, Height);
-
-		// draw light source mesh
+		// opaque
 		{
-			RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.DirectionalLightMesh.get()].get());
-			vector<shared_ptr<FHandle>> Handles;
-			Handles.push_back(MFrameRes.DirectionalLight_LocatingCB->CBHandle);
-			RHI->SetShaderInput(Handles);
-			RHI->DrawGeometry(SFrameRes.DirectionalLightMesh.get());
-		}
+			SCOPED_EVENT("Opaque");
+			RHI->SetTextureState(SFrameRes.SceneColorMap.get(), FRESOURCE_STATES::RESOURCE_STATE_RENDER_TARGET);
+			RHI->ClearRenderTarget(SFrameRes.SceneColorMap->RtvHandle.get());
+			RHI->SetRenderTarget(1, Rt, SFrameRes.DepthStencilMap->DsvHandle.get());
+			RHI->ClearDepthStencil(SFrameRes.DepthStencilMap.get());
+			RHI->SetViewport(0.0f, 0.0f, static_cast<float>(Width), static_cast<float>(Height), 0.f, 1.f);
+			RHI->SetScissor(0, 0, Width, Height);
 
-		// draw light source mesh
-		if (SFrameRes.PointLightMeshes.size() != 0)
-		{
-			RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.PointLightMeshes[0].get()].get()); // for loop use same pso, set ahead avoid extra cost
-			for (uint32 i = 0; i < SFrameRes.PointLightMeshes.size(); i++)
+			// draw light source mesh
 			{
+				RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.DirectionalLightMesh.get()].get());
 				vector<shared_ptr<FHandle>> Handles;
-				Handles.push_back(MFrameRes.PointLight_LocatingCBs[i]->CBHandle);
+				Handles.push_back(MFrameRes.DirectionalLight_LocatingCB->CBHandle);
 				RHI->SetShaderInput(Handles);
-				RHI->DrawGeometry(SFrameRes.PointLightMeshes[i].get());
+				RHI->DrawGeometry(SFrameRes.DirectionalLightMesh.get());
+			}
+
+			// draw light source mesh
+			if (SFrameRes.PointLightMeshes.size() != 0)
+			{
+				RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.PointLightMeshes[0].get()].get()); // for loop use same pso, set ahead avoid extra cost
+				for (uint32 i = 0; i < SFrameRes.PointLightMeshes.size(); i++)
+				{
+					vector<shared_ptr<FHandle>> Handles;
+					Handles.push_back(MFrameRes.PointLight_LocatingCBs[i]->CBHandle);
+					RHI->SetShaderInput(Handles);
+					RHI->DrawGeometry(SFrameRes.PointLightMeshes[i].get());
+				}
+			}
+
+			// draw character
+			{
+				RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.CharacterMesh.get()].get());
+				vector<shared_ptr<FHandle>> Handles;
+				Handles.push_back(MFrameRes.Character_ScenePass_LocatingCB->CBHandle);
+				Handles.push_back(MFrameRes.CameraCB->CBHandle);
+				Handles.push_back(MFrameRes.DirectionalLight_LightingInfoCB->CBHandle);
+				Handles.push_back(MFrameRes.PointLights_LightingInfoCB->CBHandle);
+				Handles.push_back(MFrameRes.CharacterPaletteCB->CBHandle);
+				Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
+				Handles.push_back(SFrameRes.ClampSampler->SamplerHandle);
+				RHI->SetShaderInput(Handles);
+				RHI->DrawGeometry(SFrameRes.CharacterMesh.get());
+			}
+
+			// draw static mesh
+			for (uint32 i = 0; i < SFrameRes.StaticMeshes.size(); i++)
+			{
+				if (SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()]->BlendMode == FBlendMode::TRANSLUCENT_BM)
+				{
+					continue;
+				}
+
+				//pso
+				RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()].get()); // for loop use same pso, set ahead avoid extra cost
+
+				// root signature
+				vector<shared_ptr<FHandle>> Handles;
+				Handles.push_back(SFrameRes.WarpSampler->SamplerHandle);
+				Handles.push_back(MFrameRes.StaticMesh_ScenePass_LocatingCBs[i]->CBHandle);
+				Handles.push_back(MFrameRes.CameraCB->CBHandle);
+				Handles.push_back(MFrameRes.DirectionalLight_LightingInfoCB->CBHandle);
+				Handles.push_back(MFrameRes.PointLights_LightingInfoCB->CBHandle);
+				Handles.push_back(SFrameRes.MaterialCBs[SFrameRes.StaticMeshes[i].get()]->CBHandle);
+				Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
+				for (auto j : SFrameRes.MaterialTexs[SFrameRes.StaticMeshes[i].get()])
+				{
+					Handles.push_back(j->SrvHandle);
+				}
+				RHI->SetShaderInput(Handles);
+
+				// set mesh
+				RHI->DrawGeometry(SFrameRes.StaticMeshes[i].get());
 			}
 		}
-
-		// draw character
+		// translucent
 		{
-			RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.CharacterMesh.get()].get());
-			vector<shared_ptr<FHandle>> Handles;
-			Handles.push_back(MFrameRes.Character_ScenePass_LocatingCB->CBHandle);
-			Handles.push_back(MFrameRes.CameraCB->CBHandle);
-			Handles.push_back(MFrameRes.DirectionalLight_LightingInfoCB->CBHandle);
-			Handles.push_back(MFrameRes.PointLights_LightingInfoCB->CBHandle);
-			Handles.push_back(MFrameRes.CharacterPaletteCB->CBHandle);
-			Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
-			Handles.push_back(SFrameRes.ClampSampler->SamplerHandle);
-			RHI->SetShaderInput(Handles);
-			RHI->DrawGeometry(SFrameRes.CharacterMesh.get());
-		}
-
-		// draw static mesh
-		for (uint32 i = 0; i < SFrameRes.StaticMeshes.size(); i++)
-		{
-			if (SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()]->BlendMode == FBlendMode::TRANSLUCENT_BM)
+			SCOPED_EVENT("Translucent");
+			auto Map = SFrameRes.TranslucentActorIndice;
+			for (map<float, uint32>::reverse_iterator Iter = Map.rbegin(); Iter != Map.rend(); Iter++) // make sure the opaque actor render before translucent actor
 			{
-				continue;
+				uint32 i = Iter->second;
+
+				//pso
+				RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()].get()); // for loop use same pso, set ahead avoid extra cost
+
+				// root signature
+				vector<shared_ptr<FHandle>> Handles;
+				Handles.push_back(SFrameRes.ClampSampler->SamplerHandle);
+				Handles.push_back(MFrameRes.StaticMesh_ScenePass_LocatingCBs[i]->CBHandle);
+				Handles.push_back(MFrameRes.CameraCB->CBHandle);
+				Handles.push_back(MFrameRes.DirectionalLight_LightingInfoCB->CBHandle);
+				Handles.push_back(MFrameRes.PointLights_LightingInfoCB->CBHandle);
+				Handles.push_back(SFrameRes.MaterialCBs[SFrameRes.StaticMeshes[i].get()]->CBHandle);
+				Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
+				for (auto j : SFrameRes.MaterialTexs[SFrameRes.StaticMeshes[i].get()])
+				{
+					Handles.push_back(j->SrvHandle);
+				}
+				RHI->SetShaderInput(Handles);
+
+				// set mesh
+				RHI->DrawGeometry(SFrameRes.StaticMeshes[i].get());
 			}
-
-			//pso
-			RHI->SetPipelineState(SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()].get()); // for loop use same pso, set ahead avoid extra cost
-
-			// root signature
-			vector<shared_ptr<FHandle>> Handles;
-			Handles.push_back(SFrameRes.WarpSampler->SamplerHandle);
-			Handles.push_back(MFrameRes.StaticMesh_ScenePass_LocatingCBs[i]->CBHandle);
-			Handles.push_back(MFrameRes.CameraCB->CBHandle);
-			Handles.push_back(MFrameRes.DirectionalLight_LightingInfoCB->CBHandle);
-			Handles.push_back(MFrameRes.PointLights_LightingInfoCB->CBHandle);
-			Handles.push_back(SFrameRes.MaterialCBs[SFrameRes.StaticMeshes[i].get()]->CBHandle);
-			Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
-			for (auto j : SFrameRes.MaterialTexs[SFrameRes.StaticMeshes[i].get()])
-			{
-				Handles.push_back(j->SrvHandle);
-			}
-			RHI->SetShaderInput(Handles);
-
-			// set mesh
-			RHI->DrawGeometry(SFrameRes.StaticMeshes[i].get());
-		}
-
-		auto Map = SFrameRes.TranslucentActorIndice;
-		for (map<float, uint32>::reverse_iterator Iter = Map.rbegin(); Iter != Map.rend(); Iter++) // make sure the opaque actor render before translucent actor
-		{
-			uint32 i = Iter->second;
-
-			//pso
-			RHI->SetPipelineState( SFrameRes.RRMap_ScenePass[SFrameRes.StaticMeshes[i].get()].get() ); // for loop use same pso, set ahead avoid extra cost
-
-			// root signature
-			vector<shared_ptr<FHandle>> Handles;
-			Handles.push_back(SFrameRes.ClampSampler->SamplerHandle);
-			Handles.push_back(MFrameRes.StaticMesh_ScenePass_LocatingCBs[i]->CBHandle);
-			Handles.push_back( MFrameRes.CameraCB->CBHandle );
-			Handles.push_back( MFrameRes.DirectionalLight_LightingInfoCB->CBHandle );
-			Handles.push_back( MFrameRes.PointLights_LightingInfoCB->CBHandle );
-			Handles.push_back( SFrameRes.MaterialCBs[SFrameRes.StaticMeshes[i].get()]->CBHandle );
-			Handles.push_back(SFrameRes.ShadowMap->SrvHandle);
-			for (auto j : SFrameRes.MaterialTexs[SFrameRes.StaticMeshes[i].get()])
-			{
-				Handles.push_back(j->SrvHandle);
-			}
-			RHI->SetShaderInput( Handles );
-
-			// set mesh
-			RHI->DrawGeometry( SFrameRes.StaticMeshes[i].get() );
 		}
 		RHI->SetTextureState(SFrameRes.SceneColorMap.get(), FRESOURCE_STATES::RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
 	}
